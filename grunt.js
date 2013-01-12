f5d85b2ac13b7ebc687521c5e2f637986cc7ce0b
Milos Gavrilovic <milos@gavrilovic.rs>
2013-01-12 02:30:31 +0100

    Update grunt.js with additional config

--- grunt.js
+++ grunt.js
@@ -22,10 +22,38 @@ module.exports = function(grunt) {
         dest: 'dist/<%= pkg.name %>.min.js'
       }
     },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    lint: {
+      files: ['grunt.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
     uglify: {}
   });
 
   // Default task.
-  grunt.registerTask('default', 'concat min');
+  grunt.registerTask('default', 'lint qunit concat min');
 
 };
