2dbad10a619c11f7f0499ca8092deebd8d1cef78
Milos Gavrilovic <milos@gavrilovic.rs>
2013-01-18 18:52:42 +0100

    Update watch task and added some white spaces in grunt.js

--- grunt.js
+++ grunt.js
@@ -12,13 +12,19 @@ module.exports = function(grunt) {
     },
     concat: {
       dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+        src: [
+          '<banner:meta.banner>',
+          '<file_strip_banner:src/<%= pkg.name %>.js>'
+        ],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
     min: {
       dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        src: [
+          '<banner:meta.banner>',
+          '<config:concat.dist.dest>'
+        ],
         dest: 'dist/<%= pkg.name %>.min.js'
       }
     },
@@ -26,7 +32,7 @@ module.exports = function(grunt) {
       files: ['test/**/*.html']
     },
     watch: {
-      files: 'toggler.js',
+      files: 'src/*.js',
       tasks: 'concat min'
     },
     uglify: {}
